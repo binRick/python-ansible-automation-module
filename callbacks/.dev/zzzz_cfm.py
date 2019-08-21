d4395485942a05691efe9f7d9985f9a5d2600f47
Richard Blundell <rblundell@deloitte.com>
2019-08-21 18:20:37 -0400

    add moveit checks for HEAL15039

--- callbacks/.dev/zzzz_cfm.py
+++ callbacks/.dev/zzzz_cfm.py
@@ -2,7 +2,15 @@ from __future__ import (absolute_import, division, print_function)
 import itertools, logging, os, pwd, warnings, sys, subprocess, psutil, datetime, json, tempfile, time
 from git import Repo
 from git import Git
+from oslo_serialization import jsonutils
 from ansible import __version__ as ansible_version
+
+from os.path import basename
+from ansible import constants as C
+from ansible import context
+from ansible.module_utils._text import to_text
+from ansible.utils.color import colorize, hostcolor
+from ansible.plugins.callback.default import CallbackModule as CallbackModule_default
 
 log = logging.getLogger('[MES-CFM]')
 
@@ -41,10 +49,17 @@ class CallbackModule(CallbackBase):
 	self.CFM_PUB_DEPLOY_KEY_BASE64 = os.environ['CFM_PUB_DEPLOY_KEY_BASE64']
 
     def v2_playbook_on_task_start(self, task, is_conditional):
-	pass
-	log.debug("[MES-CFM] :: TASK STARTED => {} => {}".format(task.get_name(), task._uuid))
+	#print("[MES-CFM] :: TASK STARTED => {} => {} :: action {}, \nattributes: {}\n args: {}\n diff: {}\n".format(task.get_name(), task._uuid, task.action, ", ".join(task._attributes.keys()), task._attributes['args'], task._attributes['diff']))
+	if task.action == 'template':
+		print("SAVING TEMPLATE")	
 
+    def v2_runner_on_ok(self, result, msg="ok", display_color=C.COLOR_OK):
+	#print("[MES-CFM] :: TASK OK")
+	#print(result._result)
 
+        if 'changed' in result._result and result._result['changed'] and task.action == 'template':
+		print("[MES-CFM] :: TEMPLATE TASK CHANGED")
+		
     def v2_playbook_on_start(self, playbook):
 	PRIVATE_KEY_PATH = '~/.ssh/id_rsa_cfm'
 	GIT_PATH = 'ssh://git@gitlab.product.healthinteractive.net/rblundell/mes-configuration-management'
@@ -53,10 +68,10 @@ class CallbackModule(CallbackBase):
 	GIT_BRANCH = 'master'
 	GIT_ENV = {"GIT_SSH_COMMAND": GIT_SSH_CMD}
 	GIT_CLONE_DIR = tempfile.mkdtemp()
+	COMMIT_MSG = 'automated commit'
 	if not os.path.exists(GIT_CLONE_DIR):
 	    os.makedirs(GIT_CLONE_DIR)
 
-        path = os.path.abspath(playbook._file_name)
 	print("[MES-CFM] :: PLAYBOOK STARTED")
 	print("GIT_PATH = {}".format(GIT_PATH))
 	print("GIT_CLONE_DIR = {}".format(GIT_CLONE_DIR))
@@ -65,23 +80,27 @@ class CallbackModule(CallbackBase):
 
 	repo = Repo.clone_from(GIT_PATH, GIT_CLONE_DIR, branch=GIT_BRANCH)
 
-	print("repo={}".format(repo))
+	#print("repo={}".format(repo))
 	g = Git(repo.working_dir)
 	g.checkout(GIT_BRANCH)
 	changedFiles = [ item.a_path for item in repo.index.diff(None) ]
-	print("{} changedFiles: {}".format(len(changedFiles),changedFiles))
+	#print("{} changedFiles: {}".format(len(changedFiles),changedFiles))
 	commits = list(repo.iter_commits(GIT_BRANCH))
 	latestCommitID = commits[0]
-	print("{} commits".format(len(commits)))
+	#print("{} commits".format(len(commits)))
 	latestCommit = repo.commit(latestCommitID)
 	latestCommitFiles = list(latestCommit.tree.traverse())
 	commitFiles = latestCommit.stats.files.keys()
+	
+	"""
 	print("latestCommit: {}".format(latestCommit))
 	print("{} latestCommitFiles: {}".format(len(latestCommitFiles), latestCommitFiles))
 	print("{} latestCommit files: {}".format(len(commitFiles), ", ".join(commitFiles)))
 	print("latestCommit total: {}".format(latestCommit.stats.total))
 	print("{} latestCommit files: {}".format(len(latestCommit.stats.files), latestCommit.stats.files))
+	"""
 
+	"""
 	file_count = 0
 	files = []
 	tree_count = 0
@@ -89,10 +108,10 @@ class CallbackModule(CallbackBase):
 	    file_count += item.type == 'blob'
  	    files.append(item)  
 	    tree_count += item.type == 'tree'
-
 	print("file_count={}".format(file_count))
 	print("tree_count={}".format(tree_count))
 	print("files={}".format(files))
+	"""
 
 	commitFiles = {}
 
@@ -102,12 +121,15 @@ class CallbackModule(CallbackBase):
 	    	commitFiles[C].append(f)
 	print(commitFiles)
 
-	with open("{}/{}".format(GIT_CLONE_DIR,'tf1.txt'), 'w') as fd:
+	tfile='tf2.txt'
+	with open("{}/{}".format(GIT_CLONE_DIR,tfile), 'w') as fd:
 		fd.write(str(time.time()))
+    	repo.git.add(tfile)
+	"""
         for file in repo.untracked_files:
             print('Adding untracked file: {}'.format(file))
 	    repo.git.add(file)
-
-	COMMIT_MSG = 'automated commit'
+	"""
 	repo.index.commit(COMMIT_MSG)
-	repo.git.push('origin', GIT_BRANCH)
+	pr = repo.git.push('origin', GIT_BRANCH)
+	print("push result: {}".format(pr))
